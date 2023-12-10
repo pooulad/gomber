c52fb0d332695d5a3d015f4f08bcdc8557939ca7
pooulad <poooooladi@gmail.com>
2023-12-10 19:23:49 +0330

    change checking stdin logic

--- pkg/checker.go
+++ pkg/checker.go
@@ -17,23 +17,50 @@ func Execute() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Println("Enter mobile target: ")
+	fmt.Println("Usage : ")
+	fmt.Println("Enter mobile target: [target number]")
+	fmt.Println("Enter number of requests: [number of requests]")
 
+	fmt.Println("Sample : ")
+	fmt.Println("Enter mobile target: 09191234567")
+	fmt.Println("Enter number of requests: 20")
+
+	fmt.Println("Type start and enter")
+
+	inputs := []int{}
+loop:
 	for scanner.Scan() {
+		fmt.Println(len(inputs))
+		if len(inputs) >= 2 {
+			break
+		}
+		if len(inputs) == 0 {
+			fmt.Print("Enter mobile target: ")
+		} else {
+			fmt.Print("Enter number of requests: ")
+		}
 		text := scanner.Text()
 		if text == "" {
-			fmt.Println(fmt.Errorf("please insert mobile number to start"))
+			fmt.Println(fmt.Errorf("please insert somthing"))
 			break
 		}
 
-		mobileNumber, err := strconv.Atoi(text)
+		input, err := strconv.Atoi(text)
 		if err != nil {
-			fmt.Println(fmt.Errorf("please insert mobile number to start"))
+			if len(inputs) == 0 {
+				fmt.Println(fmt.Errorf("please insert mobile number to start"))
+			} else {
+				fmt.Println(fmt.Errorf("please insert number of requests to start"))
+			}
 			break
 		}
-		SendSms(mobileNumber)
 
+		inputs = append(inputs, input)
+		goto loop
 	}
+
+	fmt.Println(inputs)
+	// SendSms(mobileNumber, amount)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal("Some problem happend in scanner")
