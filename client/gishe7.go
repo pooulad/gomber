f7650ee1ee8589fb1f68081909626bd561894e66
pooulad <poooooladi@gmail.com>
2023-12-12 01:03:32 +0330

    check gishe 7 register method too

--- client/gishe7.go
+++ client/gishe7.go
@@ -26,6 +26,24 @@ func Gishe7Request(mobileNumber int, m map[string]bool, wg *sync.WaitGroup) {
 	if resp.StatusCode == 200 {
 		m["gishe7"] = true
 		return
+	} else if resp.StatusCode == 403 {
+		resp, err := http.Post(fmt.Sprintf("https://gisheh7.ir:8080/api/user/register?mobile=0%d", mobileNumber), "application/json", nil)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		b, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(b))
+
+		if resp.StatusCode == 200 {
+			m["gishe7"] = true
+			return
+		}
+		m["gishe7"] = false
 	}
 	m["gishe7"] = false
 
